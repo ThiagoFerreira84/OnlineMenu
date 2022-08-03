f685801d12fcd02c10964eb376bd7067bebc3f5d
Thiago Ferreira <thiagokael@live.com>
2022-08-03 18:16:58 +0800

    Added unit of work

---
+++ OnlineMenu.Data/UnityOfWork.cs
@@ -0,0 +1,39 @@
+﻿using OnlineMenu.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMenu.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //ITypeRepository Type { get; }
+        
+        int Complete();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OnlineMenuEntities _context;
+
+        //public ITypeRepository Type { get; private set; }
+       
+        public UnitOfWork()
+        {
+            _context = new OnlineMenuEntities();
+            
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
