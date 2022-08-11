bbd06f467338543cafde97c13004830a250dbfaa
Thiago Ferreira <thiagokael@live.com>
2022-08-11 15:07:49 +0800

    Index view created

--- OnlineMenu.Service/ViewModels/VMCountry.cs
+++ OnlineMenu.Service/ViewModels/VMCountry.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace OnlineMenu.Service.ViewModels
 {
     public class VMCountry
     {
         public System.Guid Id { get; set; }
+
+        [Display(Name = "Country Name")]
         public string CountryName { get; set; }
+
+        [Display(Name = "Currency Symbol")]
         public string CurrencySymbol { get; set; }
+
+        [Display(Name = "Government Tax")]
         public decimal GovernmentTax { get; set; }
 
         public virtual ICollection<VMRestaurant> Restaurants { get; set; }
